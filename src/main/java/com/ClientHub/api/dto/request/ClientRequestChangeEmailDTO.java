fe08e79854ea27395d6ffde985fe41cb21b68c0f
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-01-21 09:39:37 -0500

    adapta DTOs a nuevo mobre de entidad Costumer y corrige anotacion de validacion

--- src/main/java/com/ClientHub/api/dto/request/ClientRequestChangeEmailDTO.java
+++ src/main/java/com/ClientHub/api/dto/request/ClientRequestChangeEmailDTO.java
@@ -1,7 +1,7 @@
 package com.ClientHub.api.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-public record ClientRequestChangeEmailDTO(@NotNull Integer id, @NotBlank String currentEmail,@NotBlank String newEmail) {
+public record ClientRequestChangeEmailDTO(@NotBlank @Email String newEmail) {
 }
--- src/main/java/com/ClientHub/api/dto/request/ClientRequestChangeNameDTO.java
+++ src/main/java/com/ClientHub/api/dto/request/ClientRequestChangeNameDTO.java
@@ -1,7 +1,6 @@
 package com.ClientHub.api.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record ClientRequestChangeNameDTO(@NotBlank @Email String newEmail) {
+public record ClientRequestChangeNameDTO(@NotBlank  String newName) {
 }
