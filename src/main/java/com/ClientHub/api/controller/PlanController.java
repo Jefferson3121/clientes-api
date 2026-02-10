1078276d27b650a9773e111e8f1f932ee55d83e6
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-02-10 08:18:57 -0500

    fix: camiar anotacion en PLanController de @Controller a @RestController

--- src/main/java/com/ClientHub/api/controller/PlanController.java
+++ src/main/java/com/ClientHub/api/controller/PlanController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/plan")
 public class PlanController {
 
