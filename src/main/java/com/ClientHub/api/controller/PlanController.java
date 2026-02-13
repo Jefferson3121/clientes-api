0f47fdbc7c4f72fae2a31578f96dd37111bc2dc4
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-02-13 12:36:56 -0500

    refactor(controlador): ajustar nombres de métodos en PlanController según cambios en PlanService

--- src/main/java/com/ClientHub/api/controller/PlanController.java
+++ src/main/java/com/ClientHub/api/controller/PlanController.java
@@ -4,13 +4,11 @@ import com.ClientHub.api.dto.request.PLanRequestDTO;
 import com.ClientHub.api.dto.response.PlanResponseDTO;
 import com.ClientHub.api.service.PlanService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -24,34 +22,34 @@ public class PlanController {
 
     @PostMapping
     public ResponseEntity<PlanResponseDTO> add(@Valid @RequestBody PLanRequestDTO pLanRequestDTO) {
-        PlanResponseDTO planResponseDTO = planService.add(pLanRequestDTO);
+        PlanResponseDTO planResponseDTO = planService.createPlan(pLanRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(planResponseDTO);
     }
 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id){
-        planService.delete(id);
+        planService.deletePlan(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PlanResponseDTO> getById(@PathVariable int id){
-        PlanResponseDTO plan = planService.getById(id);
+        PlanResponseDTO plan = planService.getPlanById(id);
         return ResponseEntity.status(HttpStatus.OK).body(plan);
     }
 
 
     @PatchMapping("/{id}/price")
     public ResponseEntity<Void> changePlanPrice( @PathVariable int id, @NotNull @Positive @RequestBody BigDecimal price){
-        planService.changePlanPrice(id, price);
+        planService.updatePlanPrice(id, price);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 
     @PatchMapping("/{id}/state")
     public ResponseEntity<Void> changePlanState(@PathVariable int id){
-        planService.modifyPlanState(id);
+        planService.updatePlanState(id);
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
